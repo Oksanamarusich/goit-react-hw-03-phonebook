ccb3f1fdcfd4d031c5625a80a94d92464e67a69c
Oksana.Marusich <128741626+Oksanamarusich@users.noreply.github.com>
2023-10-10 19:15:16 +0300

    Initial commit

---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+padding: 40px;
+ 
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 20px;
+
+width: 400px;
+`
+export const List = styled.ul`
+list-style: none;
+padding-left:0;
+
+`
+
+export const ListItem = styled.li`
+display: flex;
+gap: 10px;
+margin-bottom: 10px;
+
+
+`
+
+export const Button = styled.button`
+border: transparent;
+background-color: white;
+cursor:pointer;
+`
